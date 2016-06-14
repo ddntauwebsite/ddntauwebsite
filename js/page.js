ac2cd2e531f3537a505fc88faa6ce2a9e9460587
DS19 <dimitarspasovski93@gmail.com>
2016-06-14 16:06:16 +0200

    First commit

---
+++ js/page.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Dimitar on 4/9/2016.
+ */
+
+$(function(){
+
+    var lastSelectedItem = null;
+
+    $("#navbarHolder").load("./navbar.html");
+    $("#footerHolder").load("./footer.html");
+
+//acomment
+
+
+});
+
